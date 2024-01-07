453b8f3df81c15f6ced5c06f1e720dfa0b900a9c
DmytroKuriptya <dima343782@gmail.com>
2024-01-07 20:32:01 +0200

    added footer and changed templates structure

--- src/tech_shop/settings.py
+++ src/tech_shop/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from os import getenv
 
-from django.conf.global_settings import STATIC_ROOT, STATICFILES_DIRS
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
