2ee531d36b558e7cb173cae9b81224fe848df85c
DimaKuriptya <dima343782@gmail.com>
2024-01-25 23:20:36 +0200

    added js script to work with cart without page reloading

--- src/carts/urls.py
+++ src/carts/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'carts'
 
 urlpatterns = [
-    path('cart-add/<slug:product_slug>/', views.cart_add, name='cart_add'),
-    path('cart-delete/<slug:product_slug>/', views.cart_delete, name='cart_delete')
+    path('cart-add/', views.cart_add, name='cart_add'),
+    path('cart-delete/', views.cart_delete, name='cart_delete')
 ]
--- src/carts/views.py
+++ src/carts/views.py
@@ -1,27 +1,37 @@
-from django.shortcuts import get_object_or_404, redirect
+from django.http import JsonResponse
 from django.contrib import messages
-from goods.models import Product
+from django.template.loader import render_to_string
 from .models import Cart
 
 
-def cart_add(request, product_slug):
-    product = Product.objects.get(slug=product_slug)
+def cart_add(request):
+    product_id = request.POST.get('product_id')
     if request.user.is_authenticated:
-        carts = Cart.objects.filter(owner=request.user, product=product)
+        carts = Cart.objects.filter(owner=request.user, product_id=product_id)
         if carts.exists():
             cart = carts.first()
             cart.quantity += 1
             cart.save()
         else:
-            Cart.objects.create(owner=request.user, product=product)
-        messages.success(request, 'Товар успішно доданий до корзини')
-    return redirect(request.META['HTTP_REFERER'])
+            Cart.objects.create(owner=request.user, product_id=product_id)
+        # messages.success(request, 'Товар успішно додано до корзини')
+    cart_items_html = render_to_string('carts/includes/cart.html', request=request)
+    response_data = {
+        'cart_items_html': cart_items_html
+    }
+    return JsonResponse(response_data)
 
 
-def cart_delete(request, product_slug):
-    product = Product.objects.get(slug=product_slug)
+def cart_delete(request):
+    cart_id = request.POST.get('cart_id')
     if request.user.is_authenticated:
-        cart = Cart.objects.get(owner=request.user, product=product)
+        cart = Cart.objects.get(owner=request.user, id=cart_id)
+        quantity_deleted = cart.quantity
         cart.delete()
-        messages.success(request, 'Товар успішно видалено з корзини')
-    return redirect(request.META['HTTP_REFERER'])
+        # messages.success(request, 'Товар успішно видалено з корзини')
+    cart_items_html = render_to_string('carts/includes/cart.html', request=request)
+    response_data = {
+        'quantity_deleted': quantity_deleted,
+        'cart_items_html': cart_items_html
+    }
+    return JsonResponse(response_data)
